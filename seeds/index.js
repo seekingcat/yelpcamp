80ca6a5d096c9e5f40ac295354b0cb94448baaad
seekingcat <llana_a154@yahoo.co.uk>
2024-06-10 16:36:57 +0100

    added random images and basic bootstrap styling. please note the use of function to add random images as instructions in video are no longer valid

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+const axios = require('axios');
 const cities = require('./cities')
 const Campground = require('../models/campground');
 const {places, descriptors} = require('./seedHelpers');
+
 
 main().catch(err => console.log(err));
 
@@ -12,14 +14,31 @@ async function main() {
 
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
+async function seedImg() {
+  try {
+    const resp = await axios.get('https://api.unsplash.com/photos/random', {
+      params: {
+        client_id: 'xDX39DaEvVTKywBpMjR2L3_X_agdKzsByKavxkrdmQo',
+        collections: 2338626,
+      },
+    })
+    return resp.data.urls.small
+  } catch (err) {
+    console.error(err)
+  }
+}
 
 const seedDB = async() => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
      const random1000 = Math.floor(Math.random() * 1000);
+     const price = Math.floor(Math.random() * 20) + 10;
      const camp = new Campground({
         location: `${cities[random1000].city}, ${cities[random1000].state}`,
-        title: `${sample(descriptors)} ${sample(places)}`
+        title: `${sample(descriptors)} ${sample(places)}`,
+        image: await seedImg(),
+        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur debitis in omnis, vero eligendi velit eveniet soluta voluptas eius quasi. Facere, cupiditate at. Dolorem ea esse aut ducimus, inventore sit?',
+        price
      })
      await camp.save()   
     }
